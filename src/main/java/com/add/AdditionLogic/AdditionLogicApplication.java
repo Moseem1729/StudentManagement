69261f9d01d3763eff4c1b28f1dd2b04a0a69e43
Moseem1729 <pmoseem@gmail.com>
2023-04-16 19:34:07 +0530

    first commit by Moseem

---
+++ src/main/java/com/add/AdditionLogic/AdditionLogicApplication.java
@@ -0,0 +1,13 @@
+package com.add.AdditionLogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionLogicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/add/AdditionLogic/calculatar/Sum.java
@@ -0,0 +1,10 @@
+package com.add.AdditionLogic.calculatar;
+
+public class Sum {
+
+	public int sum (int a,int b) {
+		int c = a + b;
+		System.out.println("Addition service: "+ c);
+		return c;
+	}
+}
